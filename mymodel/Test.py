5cd9f0eb0eb19daba65f31a97192d0bcb2b703db
AliusFTV <kolia-miner@yandex.ru>
2023-12-31 02:50:21 +0300

    Зачистил Test, теперь весь сырой код или отдельные тесты механик можно писать туда

--- mymodel/Test.py
+++
@@ -1,9 +0,0 @@
-from transformers import BertTokenizer
-
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', return_tensors="pt")
-input_texts = ["Hello, how are you?", "I'm doing well, thank you!"]
-tokenized_data = tokenizer(input_texts, padding='max_length', max_length=1600)
-
-# Выводим информацию о размерности
-for key, value in tokenized_data.items():
-    print(f"{key}: {value.size()}")
